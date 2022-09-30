7fe2bdd827ffc4be9e43d7c6ae99ca5d6b3793b1
voelkldennis <voelkldennis@googlemail.com>
2022-09-30 16:20:29 +0200

    add Scanner and Square and Grater Method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,41 @@
-
-
-    public class Main {
+//import java.util.Scanner;
+public class Main {
         public static void main(String[] args) {
 
             String name = "Zeshan";
             myName5Times(name);
             System.out.println(name);
+            System.out.println("Bitte Namen eingeben: ");
+            name = inputName();
+            System.out.println(name);
 
         }
-
         public static String myNameIs(String name) {
             return name;
         }
-
         public static int myName5Times(String name) {
             int counter = 0;
             for (; counter < 5; counter++ ){
-                System.out.println(name + counter);
-
+                System.out.println(name + " Counter in Schleife: " + counter);
             }
             System.out.println(counter);
             return (counter);
         }
+        public static boolean valueGreater0(int value) {
+            /*
+            if (value <= 0){
+                return false;
+            }
+             */
+            return value > 0;
+        }
+
+        public static int squareNumber(int value) {
+            return value*value;
+        }
+
+        // Testen mit Scanner nicht möglich
+        public static String inputName(){
+            return new java.util.Scanner(System.in).nextLine();
+        }
     }
-
-
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
     class MainTest {
-
         @Test
         public void isNamePrinted(){
             //given
@@ -11,22 +9,58 @@ import static org.junit.jupiter.api.Assertions.*;
             //when
             String actual = Main.myNameIs(name);
             //then
-
             assertEquals("Zeshan",actual);
         }
         @Test
         public void printName5Times(){
             //givem
             String name = "Zeshan";
-
             //when
             int actual = Main.myName5Times(name);
-
             //then
             assertEquals(5,actual);
-
-
+        }
+        @Test
+        public void checkValueGreater0(){
+            //given
+            int value = 1;
+            //when
+            boolean actual = Main.valueGreater0(value);
+            //then
+            assertTrue(actual);
         }
 
+        @Test
+        public void checkValueLower0(){
+            //given
+            int value = -1;
+            //when
+            boolean actual = Main.valueGreater0(value);
+            //then
+            assertFalse(actual);
+        }
+
+        @Test
+        public void checkSquareNumberFrom20(){
+            //given
+            int value = -20;
+            //when
+            int actual = Main.squareNumber(value);
+            //then
+            assertEquals(400,actual);
+        }
+
+        /*Test funktioniert nicht da Tests nur Read Only
+        @Test
+        public void scannerCheck(){
+            //given
+            String name = "Dennis";
+            //when
+            String actual = Main.inputName();
+            //then
+            assertEquals(name,actual);
+        }
+        */
     }
+
 
